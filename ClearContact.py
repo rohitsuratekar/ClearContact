ebdddd070911bb457fec6a8cc949c9ef93c06f2c
Rohit Suratekar <rohitsuratekar@gmail.com>
2015-10-31 18:40:44 +0530

    Different deliminator.

--- ClearContact.py
+++ ClearContact.py
@@ -2,9 +2,10 @@ NameOfFile = '00002.vcf'
 
 lines = [line.rstrip('\n') for line in open(NameOfFile)]
 for line in lines:
-    if 'URL:' not in line:
-        if 'PHOTO;' not in line:
-            fh2 = open('cleraed.vcf','a')
-            fh2.write(line)
-            fh2.write('\n')
-            fh2.close()
+    if ':' in line:
+        if ';' in line:
+            if 'PHOTO;' not in line:
+                fh2 = open('cleraed.vcf','a')
+                fh2.write(line)
+                fh2.write('\n')
+                fh2.close()
