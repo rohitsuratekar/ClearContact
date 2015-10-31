c3f02aee81496ba7c15e471c11e362b460af8068
Rohit Suratekar <rohitsuratekar@gmail.com>
2015-10-31 18:37:52 +0530

    Removed URL

--- ClearContact.py
+++ ClearContact.py
@@ -2,4 +2,9 @@ NameOfFile = '00002.vcf'
 
 lines = [line.rstrip('\n') for line in open(NameOfFile)]
 for line in lines:
-    print line
+    if 'URL:' not in line:
+        if 'PHOTO;' not in line:
+            fh2 = open('cleraed.vcf','a')
+            fh2.write(line)
+            fh2.write('\n')
+            fh2.close()
