0190080989cd5ac3588976664dd2deece71c5b38
Rohit Suratekar <rohitsuratekar@gmail.com>
2015-10-31 19:13:24 +0530

    Corrected, Final

--- ClearContact.py
+++ ClearContact.py
@@ -1,11 +1,19 @@
 NameOfFile = '00002.vcf'
+numberofcontacts = 0
+newcontacts = 0
 
+listofwords = ['BEGIN:', 'VERSION:' ,'N:' , 'FN:', 'TEL;', 'END:'] #Remove field from this list if you want to remove it
 lines = [line.rstrip('\n') for line in open(NameOfFile)]
 for line in lines:
-    if ':' or ';' in line:
-        if 'ENCODING=' not in line:
-            if 'URL:' not in line:
-                fh2 = open('cleraed.vcf','a')
-                fh2.write(line)
-                fh2.write('\n')
-                fh2.close()
+    if 'BEGIN:' in line:
+        numberofcontacts = numberofcontacts +1  #For sanity check to be sure
+
+for line in lines:
+    if any(word in line for word in listofwords):
+        fh2 = open('cleraed.vcf','a')
+        fh2.write(line)
+        fh2.write('\n')
+        fh2.close()
+        if 'BEGIN:' in line:
+            newcontacts = newcontacts+1
+print ("Original contacts:%d and New Contacts:%d")%(numberofcontacts,newcontacts)
